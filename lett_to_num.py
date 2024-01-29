bd0023ea8a933b10e7c78389c837ff6c219222ab
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-01-29 22:00:41 +0700

    completing the lett to num

--- lett_to_num.py
+++ lett_to_num.py
@@ -1,1 +1,8 @@
 letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+user_input = (input('Type a Letter : ')).upper()
+
+if user_input in letter:
+    letter_index = letter.index(user_input)
+    
+print(letter_index + 1)
