e5d9ab2c4a9d8807401977d354521e790535e886
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-02-02 09:25:50 +0700

    updating the func 2

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,5 @@
+import math
+
 def plus (num_one, num_two):
     return num_one + num_two
 
@@ -10,9 +12,14 @@ def multi(num_one, num_two):
 def div(num_one, num_two):
     return num_one / num_two
 
+def sq(num_one):
+    return math.sqrt(num_one)
+
 operation = (input('Type "Add" or "Sub" or "Multi" or "Div" : ')).upper()
 
 input_num_one = int(input('Type the first number : '))
+if operation != 'sq':
+    pass
 input_num_two = int(input('Type the second number : '))
 
 if operation == 'ADD':
