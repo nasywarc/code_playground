62d91bea52dafa5952e362f0483797298ffcb3f8
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-02-03 18:10:12 +0700

    updating the func 3

--- simple_calculator.py
+++ simple_calculator.py
@@ -15,12 +15,14 @@ def div(num_one, num_two):
 def sq(num_one):
     return math.sqrt(num_one)
 
-operation = (input('Type "Add" or "Sub" or "Multi" or "Div" : ')).upper()
+def pow(num_one):
+    return math.pow(num_one, 2)
+
+operation = (input('Type "Add" or "Sub" or "Multi" or "Div" or "Sq" or "Pow" : ')).upper()
 
 input_num_one = int(input('Type the first number : '))
-if operation != 'sq':
-    pass
-input_num_two = int(input('Type the second number : '))
+if operation not in ['SQ', 'POW']:
+    input_num_two = int(input('Type the second number : '))
 
 if operation == 'ADD':
     print(plus(input_num_one, input_num_two))
@@ -28,5 +30,9 @@ elif operation == 'SUB':
     print(sub(input_num_one, input_num_two))
 elif operation == 'DIV':
     print(div(input_num_one, input_num_two))
+elif operation == 'POW':
+    print(pow(input_num_one))
+elif operation == 'SQ':
+    print(sq(input_num_one))
 else:
     print(multi(input_num_one, input_num_two))
