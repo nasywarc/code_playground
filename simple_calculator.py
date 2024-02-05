6f1c057fe51008ed7b10167d43b7131f9784d22c
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-02-05 09:19:57 +0700

    completing the program

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,4 +1,4 @@
-import math
+import math, os
 
 def plus (num_one, num_two):
     return num_one + num_two
@@ -18,21 +18,32 @@ def sq(num_one):
 def power(num_one):
     return math.pow(num_one, 2)
 
-operation = (input('Type "Add" or "Sub" or "Multi" or "Div" or "Sq" or "Pow" : ')).upper()
+os.system('cls')
+
+operation = int(input(
+'''1. Add
+2. Sub
+3. Multiply
+4. Division
+5. Square
+6. Power
+Type your choice : '''))
+
+# (input('Type "Add" or "Sub" or "Multi" or "Div" or "Sq" or "Pow" : ')).upper()
 
 input_num_one = int(input('Type the first number : '))
-if operation not in ['SQ', 'POW']:
+if operation not in [5, 6]:
     input_num_two = int(input('Type the second number : '))
 
-if operation == 'ADD':
+if operation == 1:
     print(plus(input_num_one, input_num_two))
-elif operation == 'SUB':
+elif operation == 2:
     print(sub(input_num_one, input_num_two))
-elif operation == 'DIV':
+elif operation == 4:
     print(div(input_num_one, input_num_two))
-elif operation == 'POW':
-    print(power(input_num_one))
-elif operation == 'SQ':
+elif operation == 5:
     print(sq(input_num_one))
+elif operation == 6:
+    print(power(input_num_one))
 else:
     print(multi(input_num_one, input_num_two))
