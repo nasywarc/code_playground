0127a47975639b9bc0b8acefaf4044db1b41afa9
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-02-07 18:54:07 +0700

    looping

--- simple_calculator.py
+++ simple_calculator.py
@@ -18,9 +18,11 @@ def sq(num_one):
 def power(num_one):
     return math.pow(num_one, 2)
 
-os.system('cls')
+while True:
 
-operation = int(input(
+    os.system('cls')
+
+    operation = int(input(
 '''1. Add
 2. Sub
 3. Multiply
@@ -29,19 +31,23 @@ operation = int(input(
 6. Power
 Type your choice : '''))
 
-input_num_one = int(input('Type the first number : '))
-if operation not in [5, 6]:
-    input_num_two = int(input('Type the second number : '))
+    input_num_one = int(input('Type the first number : '))
+    if operation not in [5, 6]:
+        input_num_two = int(input('Type the second number : '))
 
-if operation == 1:
-    print(plus(input_num_one, input_num_two))
-elif operation == 2:
-    print(sub(input_num_one, input_num_two))
-elif operation == 4:
-    print(div(input_num_one, input_num_two))
-elif operation == 5:
-    print(sq(input_num_one))
-elif operation == 6:
-    print(power(input_num_one))
-else:
-    print(multi(input_num_one, input_num_two))
+    if operation == 1:
+        print(plus(input_num_one, input_num_two))
+    elif operation == 2:
+        print(sub(input_num_one, input_num_two))
+    elif operation == 4:
+        print(div(input_num_one, input_num_two))
+    elif operation == 5:
+        print(sq(input_num_one))
+    elif operation == 6:
+        print(power(input_num_one))
+    else:
+        print(multi(input_num_one, input_num_two))
+        
+    continue_or_no = input('\nDo you want to continue?')
+    if continue_or_no == 'no':
+        break
