fad1b595613c2cef3fa51fd593b3aedb5b75fab5
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-02-10 12:26:17 +0700

    editing code 2

--- simple_calculator.py
+++ simple_calculator.py
@@ -22,34 +22,41 @@ while True:
 
     os.system('cls')
 
-    operation = int(input(
+    operation = input(
 '''1. Add
 2. Sub
 3. Multiply
 4. Division
 5. Square
 6. Power
-Type your choice : '''))
+Type your choice : ''')
+    
+    try:
+        operation = int(operation)
+    except:
+        print('\nYour input is invalid.')
+    if operation in [1, 2, 3, 4, 5, 6]:    
+        input_num_one = int(input('Type the first number : '))
+        if operation not in [5, 6]:
+            input_num_two = int(input('Type the second number : '))
 
-    input_num_one = int(input('Type the first number : '))
-    if operation not in [5, 6]:
-        input_num_two = int(input('Type the second number : '))
-
-    if operation == 1:
-        print(plus(input_num_one, input_num_two))
-    elif operation == 2:
-        print(sub(input_num_one, input_num_two))
-    elif operation == 4:
-        print(div(input_num_one, input_num_two))
-    elif operation == 5:
-        print(sq(input_num_one))
-    elif operation == 6:
-        print(power(input_num_one))
-    else:
-        print(multi(input_num_one, input_num_two))
-        
-    continue_or_no = input('\nDo you want to continue? ')
-    if continue_or_no != 'yes':
-        break
+        if operation == 1:
+            print(plus(input_num_one, input_num_two))
+        elif operation == 2:
+            print(sub(input_num_one, input_num_two))
+        elif operation == 3:
+            print(multi(input_num_one, input_num_two))
+        elif operation == 4:
+            print(div(input_num_one, input_num_two))
+        elif operation == 5:
+            print(sq(input_num_one))
+        elif operation == 6:
+            print(power(input_num_one))
+        else:
+            print('Your input is invalid.')
+            
+        continue_or_no = input('\nDo you want to continue? ')
+        if continue_or_no != 'yes':
+            break
 
 print('Bye!')
