c231d9c26cd4431bdd4542c9516c6a8e430d1438
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-02-22 23:51:02 +0700

    making aliases

--- gui_calculator.py
+++ gui_calculator.py
@@ -1,42 +1,42 @@
-import tkinter
+import tkinter as tk
 
-window = tkinter.Tk()
+window = tk.Tk()
 window.title('Calculator App')
 # window.minsize(width=300, height=400)
 # window.maxsize(width=300, height=400)
 # window.geometry('300x400')
 
-result = tkinter.Label(text='Hello World', height=4)
+result = tk.Label(text='Hello World', height=4)
 result.grid(row=0, column=0, columnspan=3)
 
-one = tkinter.Button(text='1', width=7, height=4)
+one = tk.Button(text='1', width=7, height=4)
 one.grid(row=1, column=0)
 
-two = tkinter.Button(text='2', width=7, height=4)
+two = tk.Button(text='2', width=7, height=4)
 two.grid(row=1, column=1)
 
-three = tkinter.Button(text='3', width=7, height=4)
+three = tk.Button(text='3', width=7, height=4)
 three.grid(row=1, column=2)
 
-four = tkinter.Button(text='4', width=7, height=4)
+four = tk.Button(text='4', width=7, height=4)
 four.grid(row=2, column=0)
 
-five = tkinter.Button(text='5', width=7, height=4)
+five = tk.Button(text='5', width=7, height=4)
 five.grid(row=2, column=1)
 
-six = tkinter.Button(text='6', width=7, height=4)
+six = tk.Button(text='6', width=7, height=4)
 six.grid(row=2, column=2)
 
-seven = tkinter.Button(text='7', width=7, height=4)
+seven = tk.Button(text='7', width=7, height=4)
 seven.grid(row=3, column=0)
 
-eight = tkinter.Button(text='8', width=7, height=4)
+eight = tk.Button(text='8', width=7, height=4)
 eight.grid(row=3, column=1)
 
-nine = tkinter.Button(text='9', width=7, height=4)
+nine = tk.Button(text='9', width=7, height=4)
 nine.grid(row=3, column=2)
 
-zero = tkinter.Button(text='0', width=7, height=4)
+zero = tk.Button(text='0', width=7, height=4)
 zero.grid(row=4, column=1)
 
 window.mainloop()
