9f98531e98eb30108d351f81e3e8eb936aa4859f
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-02-25 20:48:35 +0700

    'dark mode'

--- gui_calculator.py
+++ gui_calculator.py
@@ -2,38 +2,39 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title('Calculator App')
+window.config(background='#000')
 
-result = tk.Label(text='Hello World', height=4)
+result = tk.Label(text='Hello World', height=4, background='#000', foreground='#FFF')
 result.grid(row=0, column=0, columnspan=3)
 
-one = tk.Button(text='1', width=7, height=4)
+one = tk.Button(text='1', width=7, height=4, background='#000', foreground='#FFF')
 one.grid(row=1, column=0)
 
-two = tk.Button(text='2', width=7, height=4)
+two = tk.Button(text='2', width=7, height=4, background='#000', foreground='#FFF')
 two.grid(row=1, column=1)
 
-three = tk.Button(text='3', width=7, height=4)
+three = tk.Button(text='3', width=7, height=4, background='#000', foreground='#FFF')
 three.grid(row=1, column=2)
 
-four = tk.Button(text='4', width=7, height=4)
+four = tk.Button(text='4', width=7, height=4, background='#000', foreground='#FFF')
 four.grid(row=2, column=0)
 
-five = tk.Button(text='5', width=7, height=4)
+five = tk.Button(text='5', width=7, height=4, background='#000', foreground='#FFF')
 five.grid(row=2, column=1)
 
-six = tk.Button(text='6', width=7, height=4)
+six = tk.Button(text='6', width=7, height=4, background='#000', foreground='#FFF')
 six.grid(row=2, column=2)
 
-seven = tk.Button(text='7', width=7, height=4)
+seven = tk.Button(text='7', width=7, height=4, background='#000', foreground='#FFF')
 seven.grid(row=3, column=0)
 
-eight = tk.Button(text='8', width=7, height=4)
+eight = tk.Button(text='8', width=7, height=4, background='#000', foreground='#FFF')
 eight.grid(row=3, column=1)
 
-nine = tk.Button(text='9', width=7, height=4)
+nine = tk.Button(text='9', width=7, height=4, background='#000', foreground='#FFF')
 nine.grid(row=3, column=2)
 
-zero = tk.Button(text='0', width=7, height=4)
+zero = tk.Button(text='0', width=7, height=4, background='#000', foreground='#FFF')
 zero.grid(row=4, column=1)
 
 window.mainloop()
