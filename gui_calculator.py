d12fbeb41def741469d74575a01eaf195965babd
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-03-22 22:43:16 +0700

    change app title 2

--- gui_calculator.py
+++ gui_calculator.py
@@ -1,7 +1,7 @@
 from tkinter import Label, Button, Tk, Entry
 
 window = Tk()
-window.title('Calculator GUI')
+window.title('Calculator App')
 window.config(background='#000')
 
 result = Label(text='Results will be displayed here', height=4, background='#000', foreground='#FFF')
